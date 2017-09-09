06abfe5e45838d6e5ef0636ac0e47af94f22b5a2
Yoshi- <bue.arne@gmail.com>
2017-09-09 22:33:32 +0200

    added .gitignore

--- Helper/message.py
+++ Helper/message.py
@@ -1,5 +1,5 @@
 def createMessage(item, price):
-	msg = "[{}c/{}c - {}%] @{} Hi, I would like to buy your {} listed for {} in Harbinger (stash tab \"{}\"; position: left {}, top {}) -- {} Links: ".format(price, item.get('price'), round(((price - item.get('price')) / price) * 100), item.get('lastCharacterName'), item.get('name'), item.get('price'), item.get('stashName'), item.get('xLoc'), item.get('yLoc'), item.get('note'), item.get('Links'))
+	msg = "[{}c/{}c - {}%] @{} Hi, I would like to buy your {} listed for {} in Harbinger (stash tab \"{}\"; position: left {}, top {}) -- {} Links: {}".format(price, item.get('price'), round(((price - item.get('price')) / price) * 100), item.get('lastCharacterName'), item.get('name'), item.get('price'), item.get('stashName'), item.get('xLoc'), item.get('yLoc'), item.get('note'), item.get('Links'))
 
 	modsList = createModList(item.get('mods'))
 
